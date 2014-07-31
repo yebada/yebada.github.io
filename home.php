3ea205c4a15138675783e6de24071e48a5210039
叶八达 <yeshizhaosz@gmail.com>
2014-07-31 23:20:05 +0800

    homepage fisrt

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="main.css" />
+<title>主页</title>
+</head>
+<body>
+  <h3>About Me</h3>
+  <p class="pos_right">
+      My name is Bada Ye(叶八达 in Chinese).Now I'm a junior undergraduate of <a href="http://www.ustc.edu.cn" target="new">University of Science and Technology of China(aka USTC)</a> and major in <a href="http://cstu.ustc.edu.cn" target="new">Computer Science and Technology</a> .   
+  </p>
+  <h3>Coures 2014spring</h3>
+  <dl>
+    <dt><a href="http://staff.ustc.edu.cn/~llyue/database.html"  target="new">数据库系统及应用</a></dt>
+    <dd>3C122 1(1,2) 5(1,2)</dd>
+    <dt><a href="http://staff.ustc.edu.cn/~xyfeng/teaching/FOPL/" target="new">程序设计语言基础</a></dt>
+    <dd>3C324 1(6,7) 3(1,2)</dd>
+    <dt><a href="http://staff.ustc.edu.cn/~linlixu/ai2014spring/" target="new">人工智能基础</a></dt>
+    <dd>3C121 2(3,4) 4(3,4)</dd>
+    <dt><a href="http://staff.ustc.edu.cn/~yetian/01112801" target="new">软件工程</a></dt>
+    <dd>3C121 2(6,7,8)</dd>
+    <dt><a href="http://202.38.79.82:8940/index.html" target="new">计算机体系结构</a></dt>
+    <dd>3C221 3(6,7) 5(6,7)</dd>
+    <dt><a href="http://staff.ustc.edu.cn/~chenxjin/courses/cm/main.htm" target="new">计算方法</a></dt>
+    <dd>3A115 5(3,4,5)</dd>
+    <dt><a href="http://www.bb.ustc.edu.cn" target="new">信息安全导论</a></dt>
+    <dd>3C123 5(8,9)</dd>
+  </dl>
+</body>
+</html>
